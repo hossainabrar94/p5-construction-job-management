a977b23d7d706c84f2c7a1de1aee7416accb9cb6
Abrar <hossainabrar@aol.com>
2025-01-25 03:59:52 -0500

    updated config

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
 # Local imports
-from .config import app, db, api
+from server.config import app, db, api
 # Add your model imports
 from models import User, Project, Task, CostEstimate, Tag
 
