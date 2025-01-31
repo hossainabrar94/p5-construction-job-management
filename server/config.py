e3ff423c888a854c9068083038dd20e8ad2b5dab
Abrar <hossainabrar@aol.com>
2025-01-30 19:11:49 -0500

    back to everything working

--- server/config.py
+++ server/config.py
@@ -8,25 +8,13 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_bcrypt import Bcrypt
-# import os
 
 # Local imports
 
 # Instantiate app, set attributes
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-# app.secret_key = os.getenv('SECRET_KEY', 'supersecret')
-# app.config['SESSION_COOKIE_SAMESITE'] = 'None'   # allow cross-site
-# app.config['SESSION_COOKIE_SECURE'] = True      # True if using HTTPS
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-
-# database_url = os.getenv('DATABASE_URL')
-# if not database_url:
-#     raise ValueError("DATABASE_URL is not set! Ensure it's configured in your AWS EB environment variables.")
-# elif database_url.startswith("postgres://"):  # AWS EB sometimes uses this
-#     database_url = database_url.replace("postgres://", "postgresql://", 1)
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -44,11 +32,4 @@ api = Api(app)
 # password hash
 bcrypt = Bcrypt(app)
 
-# Instantiate CORS
-# CORS(app, supports_credentials=True, resources={
-#     r"/*": {"origins": ["http://localhost:3000"]}
-# })
 CORS(app)
-
-# app.instance_path = "/tmp/my_flask_instance"
-# os.makedirs(app.instance_path, exist_ok=True)
